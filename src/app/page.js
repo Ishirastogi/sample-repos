94fd38c9f4e7f19a9a1d3d786b2c8c58560cfc9b
Ishi Rastogi <ishi.rastogi@icloud.com>
2024-06-11 13:45:12 +0530

    Added Fronttend

---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import Header from "@/components/Header/Header";
+import HeroSection from "@/components/HeroSection/HeroSection";
+import Features from "@/components/Features/Features";
+import WhyUs from "@/components/WhyUs/WhyUs";
+import Customizable from "@/components/Customizable/Customizable";
+import Testimonials from "@/components/Testimonials/Testimonials";
+import FAQ from "@/components/FAQ/FAQ";
+import Footer from "@/components/Footer/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <Features />
+      <WhyUs />
+      <Customizable />
+      <Testimonials />
+      <FAQ />
+      <Footer />
+    </>
+  );
+}
